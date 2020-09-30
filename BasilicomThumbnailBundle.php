4b00e14facb670f4fabc477303709dd87f26041d
Vladimir Malik <vladimir@convert.no>
2020-10-01 00:23:32 +0300

    Added PackageVersionTrait for get version from Composer.

--- BasilicomThumbnailBundle.php
+++ BasilicomThumbnailBundle.php
@@ -3,9 +3,18 @@
 namespace Basilicom\ThumbnailBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 
 class BasilicomThumbnailBundle extends AbstractPimcoreBundle
 {
+    use PackageVersionTrait;
+
+    const PACKAGE_NAME = 'basilicom/thumbnail-bundle';
+
+    protected function getComposerPackageName()
+    {
+        return self::PACKAGE_NAME;
+    }
 
     public function getJsPaths()
     {
